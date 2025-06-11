ae4086b7c2b9cde5395d9ae15149e4e7f23e20a0
Your Name <ghazalchamto@gmail.com>
2025-06-11 10:36:49 +0200

    Update Employee.js

---
+++ server/models/Employee.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const employeeSchema = new mongoose.Schema({
+  employee_id: { type: String, unique: true },
+  full_name: String,
+  email: String,
+  hashed_password: String
+});
+module.exports = mongoose.model('Employee', employeeSchema);
